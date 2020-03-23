abbe7e5cfac9fa80a13eae683f4e72675310c72b
Ochuenwike Chukwudi <ochuenwikechukwudi@hotmail.com>
2020-03-23 10:55:47 -0700

    Updates

---
+++ precache-manifest.50ba3f1a5e250e28ba5adcf4d3698049.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3cc9664734a0c8b90b166f8157f3fdf4",
+    "url": "/ReactGithubFinder/index.html"
+  },
+  {
+    "revision": "23ead909d5911f031971",
+    "url": "/ReactGithubFinder/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "27ed7108597eefd42a01",
+    "url": "/ReactGithubFinder/static/css/main.affb9ef6.chunk.css"
+  },
+  {
+    "revision": "23ead909d5911f031971",
+    "url": "/ReactGithubFinder/static/js/2.effc4eb6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ReactGithubFinder/static/js/2.effc4eb6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "27ed7108597eefd42a01",
+    "url": "/ReactGithubFinder/static/js/main.fe293b34.chunk.js"
+  },
+  {
+    "revision": "e3c281c1dab9e2dbb5e1",
+    "url": "/ReactGithubFinder/static/js/runtime-main.567a229f.js"
+  }
+]);
